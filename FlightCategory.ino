6bf665fbb55e364693b9d465b3005a3adc4430bb
cantyoudobetter <michael.d.bordelon@gmail.com>
2022-12-02 09:26:51 -0600

    new api

--- FlightCategory.ino
+++ FlightCategory.ino
@@ -85,7 +85,7 @@ CRGB leds[num_leds];
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-  //WiFi.begin("Wokwi-GUEST", "", 6);
+  // WiFi.begin("Wokwi-GUEST", "", 6);
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print(".");
@@ -95,22 +95,45 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, num_leds);
   tft.begin();
   tft.setRotation(0);
-}
-void loop() {
   buildDisplay();
   buildMetar();
   LightLEDs();
-  delay(1000*60*25);
+  delay(1000*60*20);
+  ESP.restart();
 }
+
 
 
 void LightLEDs() 
 {
+  for (int x = 0; x < 50; x++) {  
+    leds[x] = CRGB::Green;
+  }
+  FastLED.show();
+  delay(500);
+  for (int x = 0; x < 50; x++) {  
+    leds[x] = CRGB::Blue;
+  }
+  FastLED.show();
+  delay(500);
+  for (int x = 0; x < 50; x++) {  
+    leds[x] = CRGB::Red;
+  }
+  FastLED.show();
+  delay(500);
+  for (int x = 0; x < 50; x++) {  
+    leds[x] = CRGB(255, 0, 255);
+  }
+  FastLED.show();
+  delay(500);
+
+
   for (int x = 0; x < num_leds; x++) {  
-    if (lArray[x].fc == 0) leds[lArray[x].led_id] = CRGB(0, 100, 0);
-    if (lArray[x].fc == 1) leds[lArray[x].led_id] = CRGB::Blue;
-    if (lArray[x].fc == 2) leds[lArray[x].led_id] = CRGB::Red;
-    if (lArray[x].fc > 2) leds[lArray[x].led_id] = CRGB::Black;
+    if (lArray[x].fc == 0) leds[lArray[x].led_id] = CRGB(0, 255, 0);
+    if (lArray[x].fc == 1) leds[lArray[x].led_id] = CRGB(0, 0, 255);
+    if (lArray[x].fc == 2) leds[lArray[x].led_id] = CRGB(255, 0, 0);
+    if (lArray[x].fc == 3) leds[lArray[x].led_id] = CRGB(255, 0, 255);
+    if (lArray[x].fc > 3) leds[lArray[x].led_id] = CRGB::Black;
   }
   // for (int x = 0; x < 50; x++) {  
   //   leds[x] = CRGB::Green;
@@ -130,7 +153,8 @@ void buildMetar()
         // char[] airport = lArray[x].airport;
         String airport_str(lArray[x].airport);
         // Serial.println(airport_str);
-        String serverPath = "https://api.checkwx.com/metar/"+airport_str+"/decoded?x-api-key=f953033170224830a32fcd07dc";
+        String serverPath = "https://avwx.rest/api/metar/"+airport_str+"?options=&airport=true&reporting=true&format=json&remove=&filter=&onfail=cache&token=hcG1LPJ-tkO0P2LIj-9VlhqkbdZNfrSICga0jcltzwM";
+        // String serverPath = "https://api.checkwx.com/metar/"+airport_str+"/decoded?x-api-key=f953033170224830a32fcd07dc";
         http.begin(serverPath.c_str());
         int httpResponseCode = http.GET();
         
@@ -138,15 +162,19 @@ void buildMetar()
           String str = http.getString(); 
           DynamicJsonDocument doc(2048);
           deserializeJson(doc, str);
-          const int res_count = doc["results"];
+          // const int res_count = doc["results"];
+          const int res_count = 1;
           // Serial.print("Res count:");
           // Serial.println(res_count);
           if (res_count > 0) {
-            const char* fc = doc["data"][0]["flight_category"];
+            const char* fc = doc["flight_rules"];
+            // const char* fc = doc["data"][0]["flight_category"];
             lArray[x].fc = 0;
             if (strcmp(fc, "VFR") == 0)  lArray[x].fc = 0;
             if (strcmp(fc, "MVFR") == 0) lArray[x].fc = 1;
             if (strcmp(fc, "IFR") == 0)  lArray[x].fc = 2;
+            if (strcmp(fc, "LIFR") == 0)  lArray[x].fc = 3;
+
           }
 
         } else {
@@ -172,19 +200,24 @@ void buildDisplay()
 {
   if(WiFi.status()== WL_CONNECTED){
     HTTPClient http;
-    String serverPath = "https://api.checkwx.com/metar/KSGR/decoded?x-api-key=f953033170224830a32fcd07dc";
+    String serverPath = "https://avwx.rest/api/metar/KSGR?options=&airport=true&reporting=true&format=json&remove=&filter=&onfail=cache&token=hcG1LPJ-tkO0P2LIj-9VlhqkbdZNfrSICga0jcltzwM";
+
     http.begin(serverPath.c_str());
     int httpResponseCode = http.GET();
-    
+    Serial.print("HTTP:");
+    Serial.println(httpResponseCode);
     if (httpResponseCode>0) {
       String str = http.getString(); 
       DynamicJsonDocument doc(2048);
       deserializeJson(doc, str);
-      const int res_count = doc["results"];
+      // const int res_count = doc["results"];
+      const int res_count = 1;
+      
       if (res_count > 0) {
-        const char* raw = doc["data"][0]["raw_text"];
+        const char* raw = doc["raw"];
         String str = String(raw);
-        Serial.println(str);
+        Serial.print("METAR:");
+        Serial.print(str);
         while (str.length() > 0)
         {
           int index = str.indexOf(' ');
@@ -203,6 +236,7 @@ void buildDisplay()
         tft.setTextColor(TFT_WHITE, TFT_BLACK);
         tft.setTextSize (3);  
         // Show the resulting substrings
+        int y_cnt = 0;
         for (int i = 2; i < StringCount; i++)
         {
           
@@ -214,15 +248,17 @@ void buildDisplay()
           if (i > 6) {break;}
           
           if (strs[i].indexOf("KT") > 0)  {
+            tft.setTextSize(4);
             tft.drawString(strs[i].substring(0,3), 10, 10);
             Serial.print("DIR:");
             Serial.println(strs[i].substring(0,3));
-            tft.drawString(strs[i].substring(3), 10, 40);
+            tft.drawString(strs[i].substring(3), 10, 60);
             Serial.print("WIND:");
             Serial.println(strs[i].substring(3));
 
           } else {
-            tft.drawString(strs[i], 10, (10+(30*(i-1))));
+            tft.setTextSize(3);            
+            tft.drawString(strs[i], 10, (110+(30*y_cnt++)));
           }
 
 
@@ -249,4 +285,11 @@ void buildDisplay()
     Serial.print("Not Connected");
   }  
 
+}
+
+void loop() {
+  // buildDisplay();
+  // buildMetar();
+  // LightLEDs();
+  // delay(1000*60);
 }
